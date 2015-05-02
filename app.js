e8b6d4b6ca0176caee8af0f12736bad5f231c667
Cameron Phillips <c.phillips6@umiami.edu>
2015-05-02 14:24:20 -0400

    Added passport authentication logic

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var admin = require('./routes/admin')
 var http = require('http');
 var path = require('path');
 var MongoStore = require('connect-mongo')(express);
-
+var passport = require('passport'), 
+LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
@@ -26,6 +27,24 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+//define login strategy
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    User.findOne({username: username }, function(err, user) {
+      if(err) {return done(err);}
+      if (!user) {
+        return done(null, false, {message: "Incorrect username."});
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, {message: 'Incorrect password.'})
+      }
+      return done(null, user);
+    });
+  }
+));
+app.use(passport.initialize());
+
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -36,6 +55,13 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/login', login.login)
 app.get('/admin', admin.admin)
+
+app.post('/login', 
+  passport.authenticate('local', { successRedirect: '/', 
+                                  failureRedirect: '/login', 
+                                  failureFlash: true}));
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
