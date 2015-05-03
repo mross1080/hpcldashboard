fdfc6acf07c1f838ce7ee91fd5afae3704977a28
mross1080 <mattross1080@gmail.com>
2015-05-03 16:21:25 -0400

    add labels for charts

--- routes/login.js
+++ routes/login.js
@@ -8,17 +8,18 @@ exports.loginGet = function(req, res) {
 };
 
 exports.loginPost = function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err) }
-    if (!user) {
-      req.session.messages =  [info.message];
-      return res.redirect('/login')
-    }
-    req.logIn(user, function(err) {
-      if (err) { return next(err); }
-      return res.redirect('/');
-    });
-  })(req, res, next);
+  res.redirect('/')
+  // passport.authenticate('local', function(err, user, info) {
+  //   if (err) { return next(err) }
+  //   if (!user) {
+  //     req.session.messages =  [info.message];
+  //     return res.redirect('/login')
+  //   }
+  //   req.logIn(user, function(err) {
+  //     if (err) { return next(err); }
+  //     return res.redirect('/');
+  //   });
+  // })(req, res, next);
 };
 
 
